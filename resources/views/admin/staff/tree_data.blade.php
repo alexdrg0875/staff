97d6e41e2b68a5ec30be42efec3d5ed15e6d6c6a
Deryhlazov Oleksandr <oleksandr.deryhlazov75@gmail.com>
2019-06-10 00:53:37 +0300

    remove ability edit employee on tree page

--- resources/views/admin/staff/tree_data.blade.php
+++ resources/views/admin/staff/tree_data.blade.php
@@ -2,7 +2,7 @@
 <ul class = "treeCSS">
     @foreach ($staff as $row)
     <li style="cursor:pointer" id="{{$row->id}}" onclick="loadTree({{$row->id}})">
-        {{$row->name}} (<b>Position: </b>{{$row->position->name}} / <b>Employment date: </b>{{$row->started_at->format('d.m.Y')}}<b> / Salary: </b> {{$row->salary}} UAH) <a href="{{$row->id}}/edit"> Edit </a>
+        {{$row->name}} (<b>Position: </b>{{$row->position->name}} / <b>Employment date: </b>{{$row->started_at->format('d.m.Y')}}<b> / Salary: </b> {{$row->salary}} UAH)
         <span id="id_{{$row->id}}"></span></li>
     @endforeach
 </ul>
